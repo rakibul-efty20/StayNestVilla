ef2b06df30c32b2a36c9133f360f1e30515694ab
efty20 <rakibulefty20@gmail.com>
2026-01-30 23:28:46 +0600

    Add project files.

---
+++ StayNestVilla_API/Controllers/VillaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StayNestVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public string GetVillas()
+        {
+            return "Here is the list of villas";
+        }
+    }
+}
